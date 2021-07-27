e2f75fcf9c1629db6827e9a1c645417ed05ae5fb
Raj Vishwakarma <rvrajvishwakarma099@gmail.com>
2021-07-27 17:44:01 +0530

    Update Audiobook.py

--- Audiobook.py
+++ Audiobook.py
@@ -1,6 +1,6 @@
 import pyttsx3
 from PyPDF2 import PdfFileReader
-book = open('E:\\Python Programs\\Python Projects\\PDA.pdf', 'rb')
+book = open('Path/PythonDefine.pdf', 'rb')
 pdfReader = PdfFileReader(book)    # object of PdfFileReader class
 
 print(pdfReader.numPages)
@@ -10,7 +10,7 @@ object = pyttsx3.init()
 page = pdfReader.getPage(2)          
 text = page.extractText()
 object.setProperty('rate',100)
-object.save_to_file(text, 'E:\\Python Programs\\Python Projects\\speech.mp3')
+object.save_to_file(text, 'Path/speech.mp3')
 object.say(text)     
 object.runAndWait()
 
